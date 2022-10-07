318bc910204a3e95a144e53f8e71f387301d38ac
Murray <1210488@bcpss.org>
2022-10-07 14:20:42 -0400

    first commit, just added a ScoreActivity

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    TextView questionTV;
+    Button firstBTN;
+    Button secondBTN;
+    Button thirdBTN;
+    Button fourthBTN;
+    Button DoneBTN;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Step 2
+        questionTV = (TextView) findViewById(R.id.QuestionTV);
+        firstBTN = (Button) findViewById(R.id.firstBTN);
+        secondBTN = (Button) findViewById(R.id.secondBTN);
+        thirdBTN = (Button) findViewById(R.id.thirdBTN);
+        fourthBTN = (Button) findViewById(R.id.fourthBTN);
+        DoneBTN = (Button) findViewById(R.id.DoneBTN);
+
+
+        firstBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "Incorrect!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        } );
+        secondBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "Incorrect!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        } );
+        thirdBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "Correct!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        } );
+        fourthBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "Incorrect!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        } );
+        DoneBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent finishedIntent = new Intent(this, ScoreActivity.class);
+                startActivity(finishedIntent);
+            }
+        });
+    }
+}
